065e0475eee714091e5c472c06722128053ef11d
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-09-19 23:59:29 +0530

    Updates

---
+++ precache-manifest.5546d0643924b1937189250c1bec0ed0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217ce8d473393bf792646d7dcebfffcd",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "58a7cec24580374f4d9c",
+    "url": "/todoList/static/css/main.f744f6ae.chunk.css"
+  },
+  {
+    "revision": "971369d619639c998f4d",
+    "url": "/todoList/static/js/2.8cfe480b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList/static/js/2.8cfe480b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a7cec24580374f4d9c",
+    "url": "/todoList/static/js/main.d690e11a.chunk.js"
+  },
+  {
+    "revision": "f8e2a15cfb8d93452ec0",
+    "url": "/todoList/static/js/runtime-main.4355a649.js"
+  },
+  {
+    "revision": "cb803bf07d19f8def15314b383ceafb4",
+    "url": "/todoList/static/media/personalLogo.cb803bf0.svg"
+  }
+]);
